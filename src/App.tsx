eb69bc1f56a417adbd1886c3e86287da17dc7568
Ethan Lemke <ethan@mfbtech.com>
2024-07-31 19:02:21 +0000

    App.tsx: add two more examples one of which shows the inconsistencies of the pspdfkit typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import "./App.css";
 import PSPDFKit, { AnnotationsUnion, List } from "pspdfkit";
 
-/*
-  EXAMPLE 1
-
-  source: https://pspdfkit.com/guides/web/importing-exporting/instant-json/
-*/
-
 //@ts-ignore ignore the unused function
-function importAndExportAnnotations(annotations: List<AnnotationsUnion>): void {
+function exampleOne(): void {
+  /*
+    EXAMPLE 1
+  
+    source: https://pspdfkit.com/guides/web/importing-exporting/instant-json/
+  */
+
   // Create and convert a rectangle annotation to Instant JSON.
   const annotation = new PSPDFKit.Annotations.RectangleAnnotation({
     pageIndex: 0,
@@ -22,17 +22,93 @@ function importAndExportAnnotations(annotations: List<AnnotationsUnion>): void {
   const annotationJSON = PSPDFKit.Annotations.toSerializableObject(annotation);
 
   /*
-  Argument of type 'AnnotationBackendJSON<RectangleAnnotationJSON, never>' is not assignable to parameter of type 'AnnotationJSONUnion'.
-  Type 'AnnotationBackendJSON<RectangleAnnotationJSON, never>' is not assignable to type 'RectangleAnnotationJSON'.
-    Type 'AnnotationBackendJSON<RectangleAnnotationJSON, never>' is not assignable to type '{ strokeWidth: number; strokeColor: string | null; fillColor: string | null; strokeDashArray?: [number, number] | null | undefined; measurementPrecision?: IMeasurementPrecision | null | undefined; measurementScale?: MeasurementScaleJSON | ... 1 more ... | undefined; lineWidth?: number | ... 1 more ... | undefined; }'.
-      Types of property 'strokeWidth' are incompatible.
-        Type 'number | undefined' is not assignable to type 'number'.
-          Type 'undefined' is not assignable to type 'number'.ts(2345)
+    error TS2345: Argument of type 'AnnotationBackendJSON<RectangleAnnotationJSON, never>' is not assignable to parameter of type 'AnnotationJSONUnion'.
+    Type 'AnnotationBackendJSON<RectangleAnnotationJSON, never>' is not assignable to type 'RectangleAnnotationJSON'.
+      Type 'AnnotationBackendJSON<RectangleAnnotationJSON, never>' is not assignable to type '{ strokeWidth: number; strokeColor: string | null; fillColor: string | null; strokeDashArray?: [number, number] | null | undefined; measurementPrecision?: IMeasurementPrecision | null | undefined; measurementScale?: MeasurementScaleJSON | ... 1 more ... | undefined; lineWidth?: number | ... 1 more ... | undefined; }'.
+        Types of property 'strokeWidth' are incompatible.
+          Type 'number | undefined' is not assignable to type 'number'.
+            Type 'undefined' is not assignable to type 'number'.
   */
   const deserializedAnnotation =
     PSPDFKit.Annotations.fromSerializableObject(annotationJSON);
 
   console.log(deserializedAnnotation);
+}
+
+//@ts-ignore ignore the unused function
+function exampleTwo(): void {
+  /*
+    EXAMPLE 2
+    
+    mixing the docs example with the pdf load function instead of the deserializer
+  */
+
+  const annRect = new PSPDFKit.Annotations.RectangleAnnotation({
+    pageIndex: 0,
+    boundingBox: new PSPDFKit.Geometry.Rect({
+      left: 100,
+      top: 150,
+      width: 200,
+      height: 75,
+    }),
+  });
+  const annRectJson = PSPDFKit.Annotations.toSerializableObject(annRect);
+  const annLine = new PSPDFKit.Annotations.LineAnnotation({
+    pageIndex: 0,
+    boundingBox: new PSPDFKit.Geometry.Rect({
+      left: 100,
+      top: 150,
+      width: 200,
+      height: 75,
+    }),
+  });
+  const annLineJson = PSPDFKit.Annotations.toSerializableObject(annLine);
+  PSPDFKit.load({
+    document: "",
+    container: "",
+    instantJSON: {
+      format: "https://pspdfkit.com/instant-json/v1",
+      annotations: [
+        /*
+          error TS2322: Type 'AnnotationBackendJSON<RectangleAnnotationJSON, never>' is not assignable to type 'AnnotationJSONUnion'.
+        */
+        annRectJson,
+        /*
+          error TS2322: Type 'AnnotationBackendJSON<LineAnnotationJSON, never>' is not assignable to type 'AnnotationJSONUnion'.
+          Type 'AnnotationBackendJSON<LineAnnotationJSON, never>' is not assignable to type 'LineAnnotationJSON'.
+            Type 'AnnotationBackendJSON<LineAnnotationJSON, never>' is not assignable to type '{ strokeWidth: number; strokeColor: string | null; fillColor: string | null; strokeDashArray?: [number, number] | null | undefined; measurementPrecision?: IMeasurementPrecision | null | undefined; measurementScale?: MeasurementScaleJSON | ... 1 more ... | undefined; lineWidth?: number | ... 1 more ... | undefined; }'.
+              Types of property 'strokeWidth' are incompatible.
+                Type 'number | undefined' is not assignable to type 'number'.
+                  Type 'undefined' is not assignable to type 'number'.
+        */
+        annLineJson,
+      ],
+    },
+  });
+}
+
+//@ts-ignore ignore the unused function
+function exampleThree(newAnnotations: List<AnnotationsUnion>): void {
+  /*
+    EXAMPLE 3
+    
+    using load with the serialized annotations from the annotation event handler
+  */
+
+  const newAnnotationJsons = newAnnotations.map(
+    PSPDFKit.Annotations.toSerializableObject
+  );
+  PSPDFKit.load({
+    document: "",
+    container: "",
+    instantJSON: {
+      format: "https://pspdfkit.com/instant-json/v1",
+      /*
+        error TS2739: Type 'List<AnnotationsBackendJSONUnion>' is missing the following properties from type 'AnnotationJSONUnion[]': length, fill, copyWithin, flat, [Symbol.unscopables]
+      */
+      annotations: newAnnotationJsons,
+    },
+  });
 }
 
 function App() {
